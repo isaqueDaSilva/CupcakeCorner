15b3890c7ef6dec25ff09ab27a0162f111167d29
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-04-05 04:44:06 -0300

    I created this HeaderSessionText view modifier for creates a custum header text for list session.

--- CupcakeCorner/Extensions/View+Extension.swift
+++ CupcakeCorner/Extensions/View+Extension.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+// MARK: - Header Session Text
+
+struct HeaderSessionText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundStyle(.black)
+    }
+}
+
+extension View {
+    func headerSessionText() -> some View {
+        self.modifier(HeaderSessionText())
+    }
+}
