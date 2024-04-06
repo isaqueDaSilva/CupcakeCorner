fc0f736134dad78cb84da0055bf8839be8bc7e40
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-04-06 03:31:29 -0300

    I created the CupcakeCard Component.

--- CupcakeCorner/View/Buy View/Components/CupcakeCard.swift
+++ CupcakeCorner/View/Buy View/Components/CupcakeCard.swift
@@ -7,32 +7,34 @@
 
 import SwiftUI
 
-struct CupcakeCard: View {
-    let name: String
-    let image: Image
-    
-    var body: some View {
-        ZStack {
-            Text(name)
-                .font(.headline)
-                .bold()
-                .frame(
-                    maxWidth: .infinity,
-                    maxHeight: .infinity,
-                    alignment: .topLeading
-                )
-                .padding([.top, .leading])
-            
-            image
-                .resizable()
-                .scaledToFit()
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundStyle(.black.opacity(0.2))
-                        .frame(height: 250)
-                )
-                .frame(height: 250)
+extension BuyView {
+    struct CupcakeCard: View {
+        let name: String
+        let image: Image
+        
+        var body: some View {
+            ZStack {
+                Text(name)
+                    .font(.headline)
+                    .bold()
+                    .frame(
+                        maxWidth: .infinity,
+                        maxHeight: .infinity,
+                        alignment: .topLeading
+                    )
+                    .padding([.top, .leading])
+                
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundStyle(.black.opacity(0.2))
+                            .frame(height: 250)
+                    )
+                    .frame(height: 250)
+            }
         }
     }
 }
