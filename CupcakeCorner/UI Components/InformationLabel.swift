b0d33798e275a5d7b3fc0162353fe6ea0e5b97b9
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-04-09 04:59:20 -0300

    I updated the SubtotalLabel for InformationLabel for display more information than only subtotal.

--- CupcakeCorner/UI Components/InformationLabel.swift
+++ CupcakeCorner/UI Components/InformationLabel.swift
@@ -9,24 +9,33 @@ import SwiftUI
 
 struct InformationLabel: View {
     let title: String
+    let isBoldText: Bool
     let subtotal: Double
     
     var body: some View {
         HStack(alignment: .top) {
-            Text(title)
-                .font(.title3)
-                .bold()
-                .frame(maxWidth: .infinity, alignment: .leading)
+            Group {
+                if isBoldText {
+                    Text(title)
+                        .font(.title3)
+                        .bold()
+                } else {
+                    Text(title)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
             
             Text(subtotal.currency)
         }
     }
     
     init(
-        title: String = "Subtotal",
-        _ subtotal: Double
+        _ subtotal: Double,
+        isBoldText: Bool = true,
+        title: String = "Subtotal"
     ) {
         self.title = title
+        self.isBoldText = isBoldText
         self.subtotal = subtotal
     }
 }
