73d8101ea5f8c275e1ca4cae5aa89298043f93d4
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-04-09 05:00:30 -0300

    I created the CheckoutView

--- CupcakeCorner/View/CheckoutView.swift
+++ CupcakeCorner/View/CheckoutView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            List {
+                Section {
+                    InformationLabel(
+                        15, 
+                        isBoldText: false,
+                        title: "Subtotal of Bag:"
+                    )
+                    
+                    InformationLabel(
+                        0,
+                        isBoldText: false,
+                        title: "Shipping:"
+                    )
+                    
+                    InformationLabel(
+                        15,
+                        isBoldText: false,
+                        title: "Total:"
+                    )
+                } header: {
+                    Text("Resume")
+                        .headerSessionText()
+                }
+                .listRowBackground(Color(uiColor: .systemGray6))
+                
+                Section {
+                    LabeledContent("Send For:", value: "Tim Cook")
+                    LabeledContent("Street:", value: "One Apple Park Way")
+                    LabeledContent("City:", value: "Cupertino")
+                    LabeledContent("Zip Code:", value: "CA 95014")
+                    LabeledContent("Payment Method:", value: "Credit Card")
+                } header: {
+                    Text("User Information")
+                        .headerSessionText()
+                }
+                .listRowBackground(Color(uiColor: .systemGray6))
+                
+                Section {
+                    Text("Payment must be made at the time of delivery of the purchase by card or cash.")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.secondary)
+                }
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Checkout")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    CancelButton { dismiss() }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    ActionButton(label: "Finish Order") { }
+                }
+            }
+        }
     }
 }
 
