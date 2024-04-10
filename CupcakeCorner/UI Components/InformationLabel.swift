c1b681429e1700da64aad4301ec74ad99748a515
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-04-09 23:27:25 -0300

    updates

--- CupcakeCorner/UI Components/InformationLabel.swift
+++ CupcakeCorner/UI Components/InformationLabel.swift
@@ -9,33 +9,17 @@ import SwiftUI
 
 struct InformationLabel: View {
     let title: String
-    let isBoldText: Bool
     let subtotal: Double
     
     var body: some View {
-        HStack(alignment: .top) {
-            Group {
-                if isBoldText {
-                    Text(title)
-                        .font(.title3)
-                        .bold()
-                } else {
-                    Text(title)
-                }
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Text(subtotal.currency)
-        }
+        LabeledContent(title, value: subtotal.currency)
     }
     
     init(
         _ subtotal: Double,
-        isBoldText: Bool = true,
         title: String = "Subtotal"
     ) {
         self.title = title
-        self.isBoldText = isBoldText
         self.subtotal = subtotal
     }
 }
