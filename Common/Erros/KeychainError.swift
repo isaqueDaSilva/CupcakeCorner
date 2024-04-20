5dde4d7330d98d96d0ab148c761f30ea3098ef14
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-04-20 03:41:11 -0300

    I created the KeychainError enum for storing some error may be occur in some Keychain request.

--- Common/Erros/KeychainError.swift
+++ Common/Erros/KeychainError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension KeychainService {
+    enum KeychainError: Error, LocalizedError {
+        case saveError
+        case noToken
+        case unexpectedTokenData
+        case unhandledError(status: OSStatus)
+        
+        var errorDescription: String? {
+            switch self {
+            case .saveError, .unexpectedTokenData:
+                NSLocalizedString("An internal error has occurred, please try again or contact us.", comment: "")
+            case .noToken:
+                NSLocalizedString("No item saved, you need to make login before to continue.", comment: "")
+            case .unhandledError(let status):
+                NSLocalizedString("An unexpected error occurred: \(status)", comment: "")
+            }
+        }
+    }
+}
