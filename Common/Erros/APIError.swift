289d0343872e58aa4125c4c113739e15f9ab02c4
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-04-20 03:39:24 -0300

    I created the API Error enum for storing the errors may be occur in some API request.

--- Common/Erros/APIError.swift
+++ Common/Erros/APIError.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum APIError: Error {
+    case badURL
+    case badEncoding
+    case badResponse
+    case incorrectAuthenticationMethod
+    case fieldsEmpty
+    case unknown(Error)
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .badURL:
+            NSLocalizedString("We are having a problem with this URL, please try again later or contact us.", comment: "")
+        case .badEncoding:
+            NSLocalizedString("Unable to encode data, please try again or contact us.", comment: "")
+        case .badResponse:
+            NSLocalizedString("There was a problem getting a response, please check your connection and try again or contact us.", comment: "")
+        case .unknown(let error):
+            NSLocalizedString("An unknown error \(error) was generated.", comment: "")
+        case .incorrectAuthenticationMethod:
+            NSLocalizedString("Unable to login, please try again.", comment: "")
+        case .fieldsEmpty:
+            NSLocalizedString("There are empty fields in the form, please fill them in before proceeding.", comment: "")
+        }
+    }
+}
