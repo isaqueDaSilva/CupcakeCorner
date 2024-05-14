81d9548d9d084d63296e00ad3f65d182fe86d94d
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-05-14 02:30:02 -0300

    I created the BagView for shows the all orders and made updates(In Admin Version)

--- Common/Views/Bag View/View/BagView.swift
+++ Common/Views/Bag View/View/BagView.swift
@@ -8,16 +8,57 @@
 import SwiftUI
 
 struct BagView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    
+    @EnvironmentObject private var cacheStorage: CacheStorageService
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         NavigationStack {
             Group {
-                BagViewPopulated()
-                //BagViewEmpty()
+                switch viewModel.viewState {
+                case .load:
+                    if !viewModel.orders.isEmpty {
+                        BagViewPopulated()
+                    } else {
+                        BagViewEmpty()
+                    }
+                case .loading:
+                    ProgressView()
+                case .faliedToLoad:
+                    BagViewEmpty(with: .error)
+                }
             }
             #if CLIENT
             .navigationTitle("Bag")
             #elseif ADMIN
             .navigationTitle("Client Orders")
+            #endif
+            .onAppear {
+                viewModel.setClientID(
+                    clientID: cacheStorage.storage[0].user?.id
+                )
+            }
+            .onChange(of: scenePhase) { oldValue, newValue in
+                if (oldValue == .inactive || oldValue == .background) && newValue == .active {
+                    if cacheStorage.storage[0].user != nil {
+                        viewModel.connect()
+                    }
+                }
+                
+                if (newValue == .background) {
+                    viewModel.disconnect()
+                }
+            }
+            #if CLIENT
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    InformationLabel(
+                        viewModel.totalOfTheBag,
+                        title: "Total of this bag"
+                    )
+                }
+            }
             #endif
         }
     }
@@ -25,4 +66,5 @@ struct BagView: View {
 
 #Preview {
     BagView()
+        .environmentObject(CacheStorageService(inMemoryOnly: true))
 }
