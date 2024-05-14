604af6cd032b63b905523cbd8084c4b284466d23
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-05-14 01:42:46 -0300

    I created the CupcakeViewLoad for displays the list of the cupcake when the list is not empty.

--- Common/Views/Cupcake View/View/Components/CupcakeViewLoad.swift
+++ Common/Views/Cupcake View/View/Components/CupcakeViewLoad.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension CupcakeView {
+    @ViewBuilder
+    func CupcakeViewLoad() -> some View {
+        ScrollView {
+            VStack {
+                #if CLIENT
+                FreeShipping()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Divider()
+                
+                if let newestCupcake = viewModel.newestCupcake {
+                    Text("New")
+                        .headerSessionText()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    NewCupcakeHighlights(
+                        name: newestCupcake.flavor,
+                        description: newestCupcake.ingredients,
+                        cover: newestCupcake.coverImage,
+                        price: newestCupcake.price
+                    ) {
+                        #if CLIENT
+                        OrderView(cupcake: newestCupcake)
+                        #elseif ADMIN
+                        CupcakeDetailView(cupcake: newestCupcake)
+                        #endif
+                    }
+                    .padding(.bottom)
+                }
+                #endif
+                         
+                #if CLIENT
+                Text("Cupcakes")
+                    .headerSessionText()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                #endif
+                
+                LazyVGrid(columns: colums) {
+                    ForEach(viewModel.cupcakes) { cupcake in
+                        NavigationLink(value: cupcake) {
+                            CupcakeCard(
+                                name: cupcake.flavor,
+                                imageData: cupcake.coverImage
+                            )
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .padding([.top, .bottom], 5)
+        }
+    }
+}
