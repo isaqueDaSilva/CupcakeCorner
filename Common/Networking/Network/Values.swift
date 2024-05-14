5e1745fec3ac1e72d76a1fa0f769d373288d5081
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-05-13 23:57:08 -0300

    I created the Values Model. This represents a value will be use in the HTTP header field

--- Common/Networking/Network/Values.swift
+++ Common/Networking/Network/Values.swift
@@ -8,10 +8,18 @@
 import Foundation
 
 extension NetworkService {
+    /// The representation of the values will be pass for a HTTP header field.
     struct Values {
+        /// The actual value will be send in a HTTP request.
         let value: String
+        
+        /// The representation of the type of value being sent in the request
         let httpHeaderField: String
         
+        /// Creates a new Value type representation
+        /// - Parameters:
+        ///   - value: The actual value will be send in a HTTP request.
+        ///   - httpHeaderField: The representation of the type of value being sent in the request
         init(value: String, httpHeaderField: HTTPHeaderField) {
             self.value = value
             self.httpHeaderField = httpHeaderField.rawValue
