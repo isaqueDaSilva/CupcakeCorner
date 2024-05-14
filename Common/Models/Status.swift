5b0e40535584ac177532bb7d64c7ddb70a339599
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-05-14 00:50:16 -0300

    I created the Status enum. This enum contains all status indicators on which the request may be.

--- Common/Models/Status.swift
+++ Common/Models/Status.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 enum Status: String, Codable, CaseIterable, Identifiable {
-    case inBag, ordered, outForDelivery, delivered, canceled
+    case inBag, ordered, outForDelivery, delivered
     
     var id: Self { self }
+    
+    var displayedName: String {
+        switch self {
+        case .ordered:
+            "Ordered"
+        case .outForDelivery:
+            "Out for Delivery"
+        case .delivered:
+            "Delivered"
+        default:
+            "Non Text for Display"
+        }
+    }
 }
