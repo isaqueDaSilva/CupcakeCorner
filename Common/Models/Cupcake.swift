c81ecf0731ba2727f351da090f140d3d0668d5ac
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-05-14 00:11:36 -0300

    I created the Cupcake model. This model stores other models that indicates how to  create, update, gets and Coding Keys to encode and decode a Cupcake.

--- Common/Models/Cupcake.swift
+++ Common/Models/Cupcake.swift
@@ -6,41 +6,82 @@
 //
 
 import Foundation
-import SwiftData
+import SwiftUI
 
-@Model
-final class Cupcake: Identifiable {
-    let id: UUID
-    var coverImage: Data
-    var flavor: String
-    var ingredients: [String]
-    var price: Double
-    let createdAt: Date
-    
-    init(
-        id: UUID,
-        coverImage: Data,
-        flavor: String,
-        ingredients: [String],
-        price: Double,
-        createdAt: Date
-    ) {
-        self.id = id
-        self.coverImage = coverImage
-        self.flavor = flavor
-        self.ingredients = ingredients
-        self.price = price
-        self.createdAt = createdAt
+struct Cupcake { }
+
+extension Cupcake {
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case coverImage = "cover_image"
+        case flavor = "flavor"
+        case ingredients = "ingredients"
+        case price = "price"
+        case createdAt = "created_at"
     }
-    
-    convenience init(for result: CupcakeResults) {
-        self.init(
-            id: result.id,
-            coverImage: result.coverImage,
-            flavor: result.flavor,
-            ingredients: result.ingredients,
-            price: result.price,
-            createdAt: result.createdAt
-        )
+}
+
+extension Cupcake {
+    struct Get: Codable, Identifiable, Equatable, Hashable {
+        let id: UUID
+        var coverImage: Data
+        var flavor: String
+        var ingredients: [String]
+        var price: Double
+        let createdAt: Date
+        
+        private init(
+            id: UUID,
+            coverImage: Data,
+            flavor: String,
+            ingredients: [String],
+            price: Double,
+            createAt: Date = .now
+        ) {
+            self.id = id
+            self.coverImage = coverImage
+            self.flavor = flavor
+            self.ingredients = ingredients
+            self.price = price
+            self.createdAt = createAt
+        }
+    }
+}
+
+extension Cupcake {
+    struct Create: Encodable {
+        var coverImage: Data
+        var flavor: String
+        var ingredients: [String]
+        var price: Double
+    }
+}
+
+extension Cupcake {
+    struct Update: Encodable {
+        let coverImage: Data?
+        let flavor: String?
+        let ingredients: [String]?
+        let price: Double?
+        
+        func encode(to encoder: any Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encodeIfPresent(self.coverImage, forKey: CodingKeys.coverImage)
+            try container.encodeIfPresent(self.flavor, forKey: CodingKeys.flavor)
+            try container.encodeIfPresent(self.ingredients, forKey: CodingKeys.ingredients)
+            try container.encodeIfPresent(self.price, forKey: CodingKeys.price)
+        }
+    }
+}
+
+extension Cupcake.Get {
+    static var sampleCupcakes: [Cupcake.Get] {
+        let image = UIImage(systemName: Icon.bag.rawValue)
+        let imageData = image?.pngData()
+        
+        return [
+            .init(id: UUID(), coverImage: imageData!, flavor: "Chocolate", ingredients: ["Chocolate", "Mental"], price: 3.0),
+            .init(id: UUID(), coverImage: imageData!, flavor: "Vanila", ingredients: ["Vanila", "Milk"], price: 3.5)
+        ]
     }
 }
