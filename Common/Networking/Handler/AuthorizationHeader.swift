fffce26af5eca4299fb629f34419d773e50436dd
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-06-24 01:03:49 -0300

    I added the documentation.

---
+++ Common/Networking/Handler/AuthorizationHeader.swift
@@ -0,0 +1,14 @@
+//
+//  AuthorizationHeader.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 20/04/24.
+//
+
+import Foundation
+
+/// Stores the all authorization headers.
+enum AuthorizationHeader: String {
+    case bearer = "Bearer"
+    case basic = "Basic"
+}
