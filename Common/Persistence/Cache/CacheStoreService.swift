7f1259c3d44de35bbc84bd73922754ea2b87306a
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-06-24 02:24:00 -0300

    I added the documentation in the CacheStoreService object.

--- Common/Persistence/Cache/CacheStoreService.swift
+++ Common/Persistence/Cache/CacheStoreService.swift
@@ -9,115 +9,19 @@ import CoreData
 import Foundation
 import SwiftUI
 
-/// An object for manager the Cache store services.
-actor CacheStoreService {
+/// A struct that provides a default cache service.
+struct CacheStoreService {
+    // MARK: - Shared Initializers -
+    
+    /// A default entrypoint for access the cache service.
     static let shared = CacheStoreService()
     
-    /// Stores the current context used.
-    private let container: NSPersistentContainer
-    
-    /// Stores the current contex for perform actions in instaces avaiables.
-    private let context: NSManagedObjectContext
-    
-    /// Returns the current context utilized by app.
-    func getContext() -> NSManagedObjectContext { context }
-    
-    /// Performing the save action, for save a new instance of some NSManagedObject.
-    func save() throws {
-        guard context.hasChanges else { return }
-        try context.save()
-    }
-    
-    /// Performing the fetch action, for find instance saved in Core Data.
-    /// - Parameter fetchResultsController: A controller that will be used for fetch instances saved in Core Data.
-    /// - Returns: Retuns the all instances saved.
-    func fetch<M: NSFetchRequestResult>(
-        _ fetchResultsController: NSFetchedResultsController<M>
-    ) throws -> [M] {
-        try fetchResultsController.performFetch()
-        
-        guard let models = fetchResultsController.fetchedObjects else {
-            return []
-        }
-        
-        return models
-    }
-    
-    func fetchWithRequest<M: NSManagedObject>(
-        for model: M.Type,
-        with entityName: String
-    ) throws -> [M] {
-        let request = NSFetchRequest<M>(entityName: entityName)
-        let models = try context.fetch(request)
-        
-        return models
-    }
-    
-    /// Fetch all changes that will be occur in the entity specified by the controller parameter.
-    /// - Parameters:
-    ///   - controller: The controller that will be used for fetching the changes occured.
-    ///   - model: The type of which model we want to search for.
-    /// - Returns: Returns the changes that occurred.
-    func fetchChanges<M: NSManagedObject>(
-        _ controller: NSFetchedResultsController<NSFetchRequestResult>,
-        by model: M.Type
-    ) -> [M] {
-        guard let models = controller.fetchedObjects as? [M] else { return [] }
-        return models
-    }
-    
-    /// Creates a new NSFetchedResultsController.
-    /// - Parameter request: Specifies which NSFetchRequest will be used in the NSFetchedResultsController
-    /// - Returns: Returns a new instace of NSFetchedResultsController created.
-    nonisolated func fetchedResultController<M: NSFetchRequestResult>(
-        _ request: NSFetchRequest<M>
-    ) -> NSFetchedResultsController<M> {
-        NSFetchedResultsController(
-            fetchRequest: request,
-            managedObjectContext: context,
-            sectionNameKeyPath: nil,
-            cacheName: nil
-        )
-    }
-    
-    /// Deleting an existing NSManagedObject.
-    /// - Parameter model: The NSManagedObject will  want delete.
-    func delete<M: NSManagedObject>(_ model: M) throws {
-        self.context.delete(model)
-        try self.save()
-    }
-    
-    private init(inMemoryOnly: Bool = false) {
-        self.container = NSPersistentContainer(name: "CacheStore")
-        
-        guard let description = container.persistentStoreDescriptions.first else {
-            fatalError("Failed to retrieve a persistent store description.")
-        }
-        
-        self.context = container.viewContext
-        
-        if inMemoryOnly {
-            description.url = URL(filePath: "/dev/null")
-        }
-        
-        container.loadPersistentStores { _, error in
-            if error != nil {
-                print("Falied to loading Book entite.")
-            }
-        }
-        
-        context.name = "main"
-        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
-    }
-}
-
-extension CacheStoreService {
+    /// Provides in memory cache service for uses in previews.
     static let preview: CacheStoreService = {
         let cacheStore = CacheStoreService(inMemoryOnly: true)
         
-        for index in 0..<10 {
-            Task {
-                let context = await cacheStore.getContext()
+        cacheStore.makeChanges { context in
+            for index in 0..<10 {
                 let cupcake = Cupcake(context: context)
                 let image = UIImage(named: Icon.bag.rawValue)
                 let imageData = image?.pngData()
@@ -140,25 +44,119 @@ extension CacheStoreService {
                 cupcake.price = Double.random(in: 0.1..<15.0)
                 cupcake.createAt = .now
                 
-                try? await cacheStore.save()
+                let bool = Bool.random()
+                let order = Order(context: context)
+                order.id = UUID()
+                order.addSprinkles = Bool.random()
+                order.deliveredTime = bool ? Date.now : nil
+                order.extraFrosting = Bool.random()
+                order.finalPrice = Double.random(in: 0.1..<20.0)
+                order.orderTime = Date.now
+                order.outForDelivery = bool ? Date.now : nil
+                order.quantity = Int16(Int.random(in: 1...20))
+                order.status = bool ? Status.delivered.displayedName : Status.ordered.displayedName
+                order.userName = "Tim Cook"
+                order.cupcake = cupcake
+                
+                do {
+                    try context.save()
+                } catch {
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
             }
-        }
-        
-        Task {
-            let context = await cacheStore.getContext()
+            
             let user = User(context: context)
             user.id = UUID()
             user.name = "Tim Cook"
             user.city = "Cupertino"
             user.email = "timcook@apple.com"
-            user.fullAdress = "One Apple Park Way, Cupertino"
+            user.fullAdress = "One Apple Park Way"
             user.zip = "CA 95014"
             user.paymentMethod = "Cash"
             user.role = "client"
-            
-            try? await cacheStore.save()
         }
         
         return cacheStore
     }()
+    
+    // MARK: - Properties -
+    /// A default persistent container.
+    private let container: NSPersistentContainer
+    
+    // MARK: - Methods -
+    
+    /// Makes a change in the container, saving, updating or deleting
+    /// a NSManagedObject.
+    func makeChanges(
+        _ action: @escaping (NSManagedObjectContext) -> Void
+    ) {
+        container.performBackgroundTask { context in
+            guard context.hasChanges else { return }
+            action(context)
+        }
+    }
+    
+    /// Makes a fetch action in the container for find all NSManagedObject saved.
+    func fetch<M: NSFetchRequestResult>(
+        _ fetchResultsController: NSFetchedResultsController<M>
+    ) throws -> [M] {
+        try fetchResultsController.performFetch()
+        
+        guard let models = fetchResultsController.fetchedObjects else {
+            return []
+        }
+        
+        return models
+    }
+    
+    func fetchWithRequest<M: NSManagedObject>(
+        for model: M.Type,
+        with entityName: String
+    ) throws -> [M] {
+        let request = NSFetchRequest<M>(entityName: entityName)
+        let models = try container.viewContext.fetch(request)
+        
+        return models
+    }
+    
+    /// Makes a fetch action in the container for find all changes in all NSManagedObject saved.
+    func fetchChanges<M: NSManagedObject>(
+        _ controller: NSFetchedResultsController<NSFetchRequestResult>,
+        by model: M.Type
+    ) -> [M] {
+        guard let models = controller.fetchedObjects as? [M] else { return [] }
+        return models
+    }
+    
+    /// Creates a new NSFetchedResultsController.
+    func fetchedResultController<M: NSFetchRequestResult>(
+        _ request: NSFetchRequest<M>
+    ) -> NSFetchedResultsController<M> {
+        NSFetchedResultsController(
+            fetchRequest: request,
+            managedObjectContext: container.viewContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+    }
+    
+    // MARK: - Internal Initializer -
+    private init(inMemoryOnly: Bool = false) {
+        self.container = NSPersistentContainer(name: "CacheStore")
+        
+        guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("Failed to retrieve a persistent store description.")
+        }
+        
+        if inMemoryOnly {
+            description.url = URL(filePath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { _, error in
+            if error != nil {
+                print("Falied to loading CacheStore entite.")
+            }
+        }
+    }
 }
