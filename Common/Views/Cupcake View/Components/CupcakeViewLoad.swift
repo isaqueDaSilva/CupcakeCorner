f25d31e7fe2639de8235f0ddf0bcd12da3d0efd2
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-07-26 23:36:44 -0300

    I made some small updates.

--- Common/Views/Cupcake View/Components/CupcakeViewLoad.swift
+++ Common/Views/Cupcake View/Components/CupcakeViewLoad.swift
@@ -11,54 +11,52 @@ import SwiftUI
 extension CupcakeView {
     @ViewBuilder
     func CupcakeViewLoad() -> some View {
-        ScrollView {
-            VStack {
-                #if CLIENT
-                HomeViewMessage()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Divider()
-                
-                if let newestCupcake = viewModel.newestCupcake {
-                    NewCupcakeHighlights(
-                        name: newestCupcake.flavor,
-                        description: newestCupcake.ingredients,
-                        cover: newestCupcake.image,
-                        price: newestCupcake.price,
-                        value: newestCupcake
-                    )
-                    .padding(.bottom)
-                }
-                
-                Text("Cupcakes")
-                    .headerSessionText()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                #endif
-                
-                LazyVGrid(columns: colums) {
-                    ForEach($viewModel.cupcakes, id: \.id) { cupcake in
-                        NavigationLink {
-                            #if CLIENT
-                            OrderView(cupcake: cupcake.wrappedValue)
-                                .environmentObject(userRepo)
-                            #elseif ADMIN
-                            CupcakeDetailView(cupcake: cupcake) { cupcakeID in
-                                viewModel.deleteCupcake(by: cupcakeID)
-                            }
-                            #endif
-                        } label: {
-                            CupcakeCard(
-                                name: cupcake.wrappedValue.flavor,
-                                image: cupcake.wrappedValue.image
-                            )
+        VStack {
+            #if CLIENT
+            HomeViewMessage()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Divider()
+            
+            if let newestCupcake = viewModel.newestCupcake {
+                NewCupcakeHighlights(
+                    name: newestCupcake.flavor,
+                    description: newestCupcake.ingredients,
+                    cover: newestCupcake.image,
+                    price: newestCupcake.price,
+                    value: newestCupcake
+                )
+                .padding(.bottom)
+            }
+            
+            Text("Cupcakes")
+                .headerSessionText()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            #endif
+            
+            LazyVGrid(columns: colums) {
+                ForEach($viewModel.cupcakes, id: \.id) { cupcake in
+                    NavigationLink {
+                        #if CLIENT
+                        OrderView(cupcake: cupcake.wrappedValue)
+                            .environmentObject(userRepo)
+                        #elseif ADMIN
+                        CupcakeDetailView(cupcake: cupcake) { cupcakeID in
+                            viewModel.deleteCupcake(by: cupcakeID)
                         }
-                        .buttonStyle(PlainButtonStyle())
+                        #endif
+                    } label: {
+                        CupcakeCard(
+                            name: cupcake.wrappedValue.flavor,
+                            image: cupcake.wrappedValue.image
+                        )
                     }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
-            .padding(.horizontal)
-            .padding([.top, .bottom], 5)
         }
+        .padding(.horizontal)
+        .padding([.top, .bottom], 5)
         #if CLIENT
         .navigationTitle("Buy")
         #elseif ADMIN
