ca9cba657b9b532dc832f245a02ef68ec9b5e05e
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-10-10 22:24:39 -0300

    Update: Some unusable methods was removed from the Order+Create DTO.

--- Common/Models/DTOs/Order/Order+Create.swift
+++ Common/Models/DTOs/Order/Order+Create.swift
@@ -11,68 +11,9 @@ extension Order {
     /// A representation of the data that used for create an order.
     struct Create: Encodable, Sendable {
         let cupcakeID: UUID
-        var quantity: Int
-        var extraFrosting: Bool
-        var addSprinkles: Bool
-        
-        private let basePrice: Double
-        
-        var extraFrostingPrice: Double {
-            Double(quantity) * 1.5
-        }
-        
-        var addSprinklesPrice: Double {
-            Double(quantity) / 2.0
-        }
-        
-        var finalPrice: Double {
-            var cupcakeCost: Double {
-                basePrice * Double(quantity)
-            }
-            
-            var extraFrostingTax: Double {
-                extraFrosting ? extraFrostingPrice : 0
-            }
-            
-            var addSprinklesTax: Double {
-                addSprinkles ? addSprinklesPrice : 0
-            }
-            
-            let finalPrice = cupcakeCost + extraFrostingTax + addSprinklesTax
-            
-            
-            return finalPrice
-        }
-        
-        enum CodingKeys: CodingKey {
-            case cupcakeID
-            case quantity
-            case extraFrosting
-            case addSprinkles
-            case finalProce
-        }
-        
-        func encode(to encoder: any Encoder) throws {
-            var container: KeyedEncodingContainer<Order.Create.CodingKeys> = encoder.container(keyedBy: Order.Create.CodingKeys.self)
-            try container.encode(self.cupcakeID, forKey: Order.Create.CodingKeys.cupcakeID)
-            try container.encode(self.quantity, forKey: Order.Create.CodingKeys.quantity)
-            try container.encode(self.extraFrosting, forKey: Order.Create.CodingKeys.extraFrosting)
-            try container.encode(self.addSprinkles, forKey: Order.Create.CodingKeys.addSprinkles)
-            try container.encode(self.finalPrice, forKey: Order.Create.CodingKeys.finalProce)
-        }
-        
-        init(
-            cupcake: UUID,
-            quantity: Int,
-            extraFrosting: Bool,
-            addSprinkles: Bool,
-            basePrice: Double
-        ) {
-            self.cupcakeID = cupcake
-            self.quantity = quantity
-            self.extraFrosting = extraFrosting
-            self.addSprinkles = addSprinkles
-            self.basePrice = basePrice
-        }
+        let quantity: Int
+        let extraFrosting: Bool
+        let addSprinkles: Bool
+        let finalPrice: Double
     }
 }
